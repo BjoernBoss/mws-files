19a3c1208e7be9e038acc6a2b5e3f0489fbe9cfd
Bjoern Boss Henrichsen <bjoernbossdev@gmail.com>
2026-06-22 00:48:02 +0200

    WIP: Integrate new API for interacting with the raw files

--- files.ts
+++ files.ts
@@ -4,20 +4,26 @@ import * as mws from "@bjoernboss/mws";
 import * as libFsPromises from "fs/promises";
 import * as libFs from "fs";
 
-const maxUploadSize = 100_000_000;
+const MAX_UPLOAD_SIZE = 100_000_000;
 
-/*	Directory templates
-*	page defines:
-*		{path}: path of directory
-*		{basepath}: base path of the share module
-*		{entries}: appended list of children
-*	Entry defines:
-*		{path}: path to entry
-*		{name}: name of entry
-*		{type}: 'file' or 'dir'
-*	Empty defines:
-*		%none%
-*/
+/**
+ *	Endpoints used by the module.
+ *	This mapping can be used to translate components of the module to different paths in the URL space.
+ */
+export const Endpoints = {
+	/** directory containting static assets (sparsely used) */
+	static: '/static',
+
+	/** directory for viewing files */
+	lobby: '/view',
+
+	/** directory for raw files (GET, DELETE, POST; fully owned, auto-responds with 404) */
+	files: '/raw',
+
+	/** directory for web-sockets */
+	sockets: '/ws'
+}
+
 export class FileShare extends mws.ModuleHandler {
 	private templates: { entry: string, empty: string, page: string };
 	private fileStorage: (path: string) => string;
@@ -58,8 +64,6 @@ export class FileShare extends mws.ModuleHandler {
 		if (hasParent || content.length > 0) {
 			const teEntry = this.templates.entry;
 
-			mws.expandPlaceholders
-
 			/* add the parent entry */
 			if (hasParent) {
 				const parentPath = dirPath.substring(0, dirPath.lastIndexOf('/', dirPath.length - 2));
@@ -89,34 +93,129 @@ export class FileShare extends mws.ModuleHandler {
 		const out = mws.expandPlaceholders(this.templates.page, { path: client.path, basepath: '.', entries: entries }, false);
 		client.respond(out, { media: mws.Media.Html });
 	}
-	private async deleteFile(client: mws.ClientRequest, filePath: string): Promise<void> {
-		try {
-			const stat = await libFsPromises.stat(filePath);
-			if (!stat.isFile()) {
-				client.respondNotFound();
-				return;
+
+	private async fetchDirectoryState(filePath: string): Promise<string> {
+		const out: Record<string, { kind: string, size: number, modified: number }> = {};
+
+		/* let errors propagate outward */
+		const content = await libFsPromises.readdir(filePath);
+
+		/* collect all of the meta data about the directory (silently ignore errors) */
+		for (const name of content) {
+			const childPath = `${filePath}/${name}`;
+			try {
+				const stats = await libFsPromises.stat(childPath);
+
+				if (stats.isFile())
+					out[name] = { kind: 'file', size: stats.size, modified: stats.mtimeMs };
+				else if (stats.isDirectory())
+					out[name] = { kind: 'directory', size: (await libFsPromises.readdir(childPath)).length, modified: stats.mtimeMs };
+				else
+					this.warning(`Unsupported file-system object encountered: ${childPath}`);
 			}
-			await libFsPromises.unlink(filePath);
-			client.respondOk({ message: `File was deleted successfully` });
-		} catch (err: any) {
-			if (err.code == 'ENOENT') {
-				client.respondNotFound();
-				return;
+			catch (err: any) {
+				this.warning(`Error fetching information about directory entry [${childPath}]: ${err.message}`);
 			}
-			client.respondInternalError(`Filesystem error while deleting [${filePath}]: ${err.message}`);
 		}
+		return JSON.stringify(out);
 	}
-	private async uploadFile(client: mws.ClientRequest, filePath: string): Promise<void> {
+	private async handleUpload(client: mws.ClientRequest, filePath: string): Promise<void> {
+		const kind = client.url.searchParams.get('kind') ?? 'file';
+		if (kind != 'file' && kind != 'directory')
+			return client.respondBadRequest(`Unsupported kind [${kind}] encountered`);
+
 		try {
-			await client.receiveToFile(filePath, maxUploadSize);
-			client.respondOk({ message: 'File was successfully uploaded' });
+			/* check if a directory is to be created */
+			if (kind == 'directory') {
+				await libFsPromises.mkdir(filePath, { recursive: false });
+				return client.respondOk({ message: `Directory created` });
+			}
+
+			/* try to upload the file */
+			else if (!await this.cache.write(filePath, client.receiveData(MAX_UPLOAD_SIZE), { create: true }))
+				return client.respondConflict(`Path already exists`);
+			return client.respondOk({ message: `File uploaded` });
 		}
 		catch (err: any) {
-			client.error(`Failed to upload file: ${err.message}`);
+			if (err.code == 'ENOENT')
+				return client.respondBadRequest(`Path for ${kind} does not exist`);
+			if (err.code == 'EEXIST')
+				return client.respondConflict(`Path already exists`);
+			return client.respondInternalError(`Failed to create ${kind} [${filePath}]: ${err}`);
 		}
 	}
+	private async handleDelete(client: mws.ClientRequest, filePath: string): Promise<void> {
+		const kind = client.url.searchParams.get('kind') ?? 'file';
+		if (kind != 'file' && kind != 'directory')
+			return client.respondBadRequest(`Unsupported kind [${kind}] encountered`);
+
+		/* try to remove the object */
+		try {
+			if (kind == 'directory')
+				await libFsPromises.rmdir(filePath);
+			else if (!await this.cache.remove(filePath))
+				return client.respondNotFound();
+			client.respondOk({ message: `${kind[0].toUpperCase()}${kind.substring(1)} successfully deleted` });
+		}
+		catch (err: any) {
+			if (kind == 'directory' && err.code == 'ENOTEMPTY')
+				return client.respondConflict('Directory not empty');
+
+			/* check if its a kind mis-match */
+			try {
+				const stats = await libFsPromises.stat(filePath);
+				if (!stats.isFile() && !stats.isDirectory())
+					this.warning(`Unsupported file-system object encountered: ${filePath}`);
+				if (!(kind == 'directory' ? stats.isDirectory() : stats.isFile()))
+					return client.respondConflict(`Path is not a ${kind}`);
+			}
+			catch (_err: any) {
+				if (_err.code == 'ENOENT')
+					return client.respondNotFound();
+			}
+			client.respondInternalError(`Failed to remove file [${filePath}]: ${err.message}`);
+		}
+	}
+	private async handleFiles(client: mws.ClientRequest, path: string): Promise<void> {
+		const filePath = this.fileStorage(path);
+
+		/* ensure the request is using a supported method */
+		const method = client.requireMethod(['GET', 'POST', 'DELETE']);
+		if (method == null)
+			return;
+
+		/* check if the method is allowed for the given endpoint */
+		if (path == '/' && method != 'GET')
+			return client.respondForbidden('Root cannot be modified', { forwardReason: true });
+
+		/* check if the entry is to be deleted or uploaded */
+		if (method == 'DELETE')
+			return this.handleDelete(client, filePath);
+		else if (method == 'POST')
+			return this.handleUpload(client, filePath);
+
+		try {
+			const stats = await libFsPromises.stat(filePath);
+
+			/* server the directory or file */
+			if (stats.isDirectory())
+				return client.respond(await this.fetchDirectoryState(filePath), { media: mws.Media.Json, status: mws.Status.Ok, headers: { 'File-Kind': 'directory' } });
+			else if (!stats.isFile())
+				this.warning(`Unsupported file-system object encountered: ${filePath}`);
+			else if (await client.tryRespondFile(filePath, { checkFreshness: true, headers: { 'File-Kind': 'file' } }))
+				return;
+		} catch (err: any) {
+			if (err.code != 'ENOENT')
+				client.respondInternalError(`Failed to serve path [${filePath}]: ${err.message}`);
+		}
+		client.respondNotFound();
+	}
 	protected override async handleRequest(client: mws.ClientRequest): Promise<void> {
-		client.trace(`Shared handler for [${client.path}]`);
+		client.trace(`Files handler for [${client.path}]`);
+
+		/* check if its a request for the raw files API (allow root itself for reading it) */
+		if (client.isSubPathOf(Endpoints.files))
+			return this.handleFiles(client, client.getChildPath(Endpoints.files));
 
 		/* expand the path */
 		const filePath = this.fileStorage(client.path);
@@ -125,14 +224,6 @@ export class FileShare extends mws.ModuleHandler {
 		const method = client.requireMethod(['GET', 'POST', 'DELETE']);
 		if (method == null)
 			return;
-
-		/* handle file uploads */
-		if (method == 'POST')
-			return this.uploadFile(client, filePath);
-
-		/* handle file deletion */
-		if (method == 'DELETE')
-			return this.deleteFile(client, filePath);
 
 		/* check if the path exists in the filesystem */
 		try {
